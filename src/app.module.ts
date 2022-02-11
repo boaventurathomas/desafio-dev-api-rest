e4c30504128fefd32531597fd669d5db473444ff
Thomas Boa Ventura <boaventura.thomas@gmail.com>
2022-02-11 16:18:43 -0300

    chore: config database mysql typeorm  connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PortadorModule } from './application/portador/portador.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'desafio-dev-api-rest',
+      entities: [],
+      synchronize: true,
+    }),
+    PortadorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
