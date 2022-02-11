b83d669eb01632f7fe7eb14e636938a32026223a
Thomas Boa Ventura <boaventura.thomas@gmail.com>
2022-02-11 19:32:00 -0300

    feat: create DTO to portador create

--- src/application/portador/dto/create-portador.dto.ts
+++ src/application/portador/dto/create-portador.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePortadorDto {}
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreatePortadorDto {
+  @IsString()
+  @IsNotEmpty()
+  nome: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(11, 11)
+  cpf: string;
+}
