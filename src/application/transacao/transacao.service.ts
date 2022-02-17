802f27bc32323986191f34393e03fdfbf7e20ee4
Thomas Boa Ventura <boaventura.thomas@gmail.com>
2022-02-17 18:25:20 -0300

    refac: use functions in saque

--- src/application/transacao/transacao.service.ts
+++ src/application/transacao/transacao.service.ts
@@ -27,7 +27,11 @@ export class TransacaoService {
       .andWhere('valor < 0')
       .getRawOne()
 
-    return transacoesDia.totalDia === null ? 0 : transacoesDia.totalDia * -1;
+    if (transacoesDia.totalDia === null) {
+      return 0
+    } else {
+      return transacoesDia.totalDia * -1;
+    }
   }
 
   validaLimite(conta: Conta, valorTransacao: number, valorTotalResgatadoDia: number): boolean {
@@ -89,27 +93,27 @@ export class TransacaoService {
     await queryRunner.startTransaction();
 
     try {
-      const transacaoRepository = connection.getRepository(Transacao);
-      const contaRepository = connection.getRepository(Conta);
+      const transacaoRepository: Repository<Transacao> = connection.getRepository(Transacao);
+      const contaRepository: Repository<Conta> = connection.getRepository(Conta);
       const valorSaque = +saqueDto.valor;
 
-      const conta = await contaRepository.findOne({ agencia: saqueDto.conta.agencia, conta: saqueDto.conta.conta });
+      const contaCadastrada = await this.buscaConta(saqueDto.conta, contaRepository);
 
-      if (!conta) {
+      if (!contaCadastrada) {
         return notFound(`Agência ${saqueDto.conta.agencia} e conta ${saqueDto.conta.conta} não cadastrada`)
       }
 
-      if (!this.contaAtiva(conta)) {
-        return badRequest(`Agência ${conta.agencia} e conta ${conta.conta} inativa`)
+      if (!this.contaAtiva(contaCadastrada)) {
+        return badRequest(`Agência ${contaCadastrada.agencia} e conta ${contaCadastrada.conta} inativa`)
       }
 
-      if (!this.validaSaldoContaTransacao(conta, valorSaque)) {
+      if (!this.validaSaldoContaTransacao(contaCadastrada, valorSaque)) {
         return badRequest('Saldo insuficiente')
       }
 
-      const valorTotalResgatadoDia = await this.consultaValorSaqueDia(conta.agencia, conta.conta, getDateNowToDB());
+      const valorTotalResgatadoDia = await this.consultaValorSaqueDia(contaCadastrada.agencia, contaCadastrada.conta, getDateNowToDB());
 
-      if (!this.validaLimite(conta, valorSaque, valorTotalResgatadoDia)) {
+      if (!this.validaLimite(contaCadastrada, valorSaque, valorTotalResgatadoDia)) {
         return badRequest('Limite diário atingido')
       }
 
@@ -118,12 +122,9 @@ export class TransacaoService {
       const createdTransacao = transacaoRepository.create(saqueDto)
       await transacaoRepository.save(createdTransacao)
 
-      const novoSaldo = (+conta.saldo) - valorSaque;
+      const novoSaldo = (+contaCadastrada.saldo) - valorSaque;
 
-      await contaRepository.update({
-        conta: conta.conta,
-        agencia: conta.agencia
-      }, { saldo: novoSaldo });
+      await this.atualizaSaldoConta(contaCadastrada, novoSaldo, contaRepository)
 
       await queryRunner.commitTransaction();
 
@@ -134,8 +135,8 @@ export class TransacaoService {
           valor: createdTransacao.valor
         },
         conta: {
-          conta: conta.conta,
-          agencia: conta.agencia,
+          conta: contaCadastrada.conta,
+          agencia: contaCadastrada.agencia,
           saldo: novoSaldo
         }
       }
@@ -159,8 +160,9 @@ export class TransacaoService {
 
     try {
 
-      const transacaoRepository = connection.getRepository(Transacao);
+      const transacaoRepository: Repository<Transacao> = connection.getRepository(Transacao);
       const contaRepository: Repository<Conta> = connection.getRepository(Conta);
+
       const contaCadastrada = await this.buscaConta(depositoDto.conta, contaRepository);
 
       if (!contaCadastrada) {
